38a5b54521a9918db16d84cc5b6349c5d2211dbe
JackeL <it.davidkaiser@gmail.com>
2017-06-11 18:03:36 -0700

    bug fix

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -38,7 +38,7 @@
             <th>Port</th>
             <th>TCP</th>
             <th>UDP</th>
-            <th>Description</th>
+            <th>Known Services</th>
           </tr>
         </thead>
         <tbody>
@@ -46,7 +46,14 @@
             <td>{{ item.port }}</td>
             <td>{{ item.tcp }}</td>
             <td>{{ item.udp }}</td>
-            <td>{{ item.description }}</td>
+            <template v-if="typeof item.description === 'string'">
+              <td><ul>
+                <li>{{ item.description }}</li></ul></td>
+            </template>
+            <template v-else>
+              <td><ul>
+                <li v-for="desc in item.description">{{ desc }}</li></ul></td>
+            </template>
           </tr>
         </tbody>
       </table>
@@ -99,16 +106,17 @@
               portstatus: ''
             })
           } else {
+            this.jsonarr.push({
+              portindex: port,
+              port: port,
+              status: this.jsonports.ports[port][0].status,
+              udp: this.jsonports.ports[port][0].udp,
+              tcp: this.jsonports.ports[port][0].tcp,
+              description: [],
+              portstatus: ''
+            })
             for (let x = 0; x < this.jsonports.ports[port].length; x++) {
-              this.jsonarr.push({
-                portindex: port,
-                port: this.jsonports.ports[port][x].port,
-                description: this.jsonports.ports[port][x].description,
-                status: this.jsonports.ports[port][x].status,
-                udp: this.jsonports.ports[port][x].udp,
-                tcp: this.jsonports.ports[port][x].tcp,
-                portstatus: ''
-              })
+              this.jsonarr[this.jsonarr.length - 1].description.push(this.jsonports.ports[port][x].description)
             }
           }
         })
@@ -117,14 +125,16 @@
         this.scancompleted = false
         let promises = []
         let count = 0
+        let prev = ''
         this.jsonarr.forEach((item, index) => {
           promises.push(new Promise((resolve, reject) => {
             this.$portscanner.checkPortStatus(item.portindex, '127.0.0.1', (error, status) => {
               // Status is 'open' if currently in use or 'closed' if available
               item.portstatus = status
-              if (status === 'open') {
+              if (status === 'open' && prev !== item.portindex) {
                 count++
               }
+              prev = item.portindex
               if (error) console.log('error')
               resolve()
             })
